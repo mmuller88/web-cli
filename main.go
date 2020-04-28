f308d8c09af32c923f1643c290896f1412922f91
Martin Müller <damadden88@googlemail.com>
2020-04-28 08:09:02 +0200

    take auth and permisison

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 import (
 	"fmt"
+	"github.com/mmcdole/gofeed"
 )
 func main(){
-	fmt.Println("Eine Zeile Text")
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
+	for i := 0; i < 5; i++ {
+		fmt.Println(feed.Items[i].Title)
+	}
 }
